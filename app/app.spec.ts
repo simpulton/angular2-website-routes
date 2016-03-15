fd6be62d852bb37dbbcf440b2f311eba422bdd43
Jonathan Garvey <jon.d.garvey@gmail.com>
2016-03-15 16:01:07 -0700

    feature(testing): set up webpack-karma testing environment

---
+++ app/app.spec.ts
@@ -0,0 +1,9 @@
+import { describe, it, expect } from 'angular2/testing';
+
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  it('should be a function', () => {
+    expect(typeof AppComponent).toBe('function');
+  });
+});
